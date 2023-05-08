03bcc16ce08e8c1a7dc52b98ac3a7c9f1bf1f8b2
Jessica <fr_jess@icloud.com>
2023-05-08 08:46:37 +0200

    install vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+  name: 'App',
+
+  data(){
+    return{
+      titolo: 'Mars Rover'
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <div class="container">
+    <h1>{{titolo}}</h1>
+  </div>
+
+
+</template>
